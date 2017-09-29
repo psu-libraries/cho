dde8a212981e222a5517371ea2ee67eb925819d2
Adam Wead <amsterdamos@gmail.com>
2017-09-29 15:20:49 -0400

    Adding Coveralls
    Checks for test coverage on PRs and commits.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,19 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
+
+if ENV['COVERAGE'] || ENV['TRAVIS']
+  require 'simplecov'
+  require 'coveralls'
+  SimpleCov.root(File.expand_path('../..', __FILE__))
+  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+  SimpleCov.start('rails') do
+    add_filter '/spec'
+    add_filter '/tasks'
+  end
+  SimpleCov.command_name 'spec'
+end
+
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
