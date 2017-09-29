1c35d705d60ee4c7b9d41e6d261cfdd5aa6c5194
Carolyn Cole <cam156@psu.edu>
2017-09-29 11:18:48 -0400

    Adding in factory girl

---
+++ spec/factories/metadata_application_profile_field.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+FactoryGirl.define do
+  factory :metadata_application_profile_field do
+    sequence(:id)
+    label 'abc123_label'
+    field_type 'date'
+    requirement_designation 'recommended'
+    controlled_vocabulary 'abc123_vocab'
+    default_value 'abc123'
+    display_name 'My Abc123'
+    display_transformation 'abc123_transform'
+    multiple false
+    validation 'abc123_validation'
+  end
+end
