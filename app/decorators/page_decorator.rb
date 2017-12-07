3816822821ef5a50504ef2623789f3115baaa51a
Adam Wead <amsterdamos@gmail.com>
2017-12-07 10:03:12 -0500

    Remove test directory; fix rubocop

--- app/decorators/page_decorator.rb
+++ app/decorators/page_decorator.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class PageDecorator < Draper::Decorator
   delegate_all
 
@@ -9,5 +11,4 @@ class PageDecorator < Draper::Decorator
   #       object.created_at.strftime("%a %m/%d/%y")
   #     end
   #   end
-
 end
--- test/controllers/pages_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class PagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get HelloReact" do
-    get pages_HelloReact_url
-    assert_response :success
-  end
-
-end
