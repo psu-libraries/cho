264dc8fc70264f55cd0eec71faafc8318a472ac4
Adam Wead <amsterdamos@gmail.com>
2017-12-07 11:00:39 -0500

    Clickable horsey!

--- app/javascript/chess/Board.jsx
+++ app/javascript/chess/Board.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Square from './Square';
 import Knight from './Knight';
+import { canMoveKnight, moveKnight } from './Game';
 
 export default class Board extends Component {
   renderSquare(i) {
@@ -16,12 +17,19 @@ export default class Board extends Component {
 
     return (
       <div key={i}
-           style={{ width: '12.5%', height: '12.5%' }}>
+           style={{ width: '12.5%', height: '12.5%' }}
+           onClick={() => this.handleSquareClick(x, y)}>
         <Square black={black}>
           {piece}
         </Square>
       </div>
     );
+  }
+
+  handleSquareClick(toX, toY) {
+    if (canMoveKnight(toX, toY)) {
+      moveKnight(toX, toY);
+    }
   }
 
   render() {
--- app/javascript/chess/Game.jsx
+++ app/javascript/chess/Game.jsx
@@ -1,8 +1,29 @@
+let knightPosition = [1, 7];
+let observer = null;
 
+function emitChange() {
+  observer(knightPosition);
+}
 
-export function observe(receive) {
-  setInterval(() => receive([
-    Math.floor(Math.random() * 8),
-    Math.floor(Math.random() * 8)
-  ]), 500);
+export function observe(o) {
+  if (observer) {
+    throw new Error('Multiple observers not implemented.');
+  }
+
+  observer = o;
+  emitChange();
+}
+
+export function moveKnight(toX, toY) {
+  knightPosition = [toX, toY];
+  emitChange();
+}
+
+export function canMoveKnight(toX, toY) {
+  const [x, y] = knightPosition;
+  const dx = toX - x;
+  const dy = toY - y;
+
+  return (Math.abs(dx) === 2 && Math.abs(dy) === 1) ||
+         (Math.abs(dx) === 1 && Math.abs(dy) === 2);
 }
