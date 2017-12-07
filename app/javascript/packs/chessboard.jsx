d3d29aeb6aefc4d3c1882e8ed0b5b191e90e876a
Adam Wead <amsterdamos@gmail.com>
2017-12-07 10:11:50 -0500

    Adding Square class

--- app/javascript/packs/chessboard.jsx
+++ app/javascript/packs/chessboard.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Knight from '../chess/Knight';
+import Square from '../chess/Square';
 
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<Knight />, document.getElementById('chessboard'))
+  ReactDOM.render(
+    <Square black>
+      <Knight />
+    </Square>,
+    document.getElementById('chessboard')
+  )
 })
