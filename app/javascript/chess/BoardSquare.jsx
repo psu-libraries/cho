3b9d77024e12d0bf802c2e9217e48767b779fa87
Adam Wead <amsterdamos@gmail.com>
2017-12-07 11:18:28 -0500

    Draggable (invalid) horsey! ♞

---
+++ app/javascript/chess/BoardSquare.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Square from './Square';
+import { canMoveKnight, moveKnight } from './Game';
+import { ItemTypes } from './Constants';
+import { DropTarget } from 'react-dnd';
+
+const squareTarget = {
+  drop(props) {
+    moveKnight(props.x, props.y);
+  }
+};
+
+function collect(connect, monitor) {
+  return {
+    connectDropTarget: connect.dropTarget(),
+    isOver: monitor.isOver()
+  };
+}
+
+class BoardSquare extends Component {
+  render() {
+    const { x, y, connectDropTarget, isOver } = this.props;
+    const black = (x + y) % 2 === 1;
+
+    return connectDropTarget(
+      <div style={{
+        position: 'relative',
+        width: '100%',
+        height: '100%'
+      }}>
+        <Square black={black}>
+          {this.props.children}
+        </Square>
+        {isOver &&
+          <div style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            height: '100%',
+            width: '100%',
+            zIndex: 1,
+            opacity: 0.5,
+            backgroundColor: 'yellow',
+          }} />
+        }
+      </div>
+    );
+  }
+}
+
+BoardSquare.propTypes = {
+  x: PropTypes.number.isRequired,
+  y: PropTypes.number.isRequired,
+  connectDropTarget: PropTypes.func.isRequired,
+  isOver: PropTypes.bool.isRequired
+};
+
+export default DropTarget(ItemTypes.KNIGHT, squareTarget, collect)(BoardSquare)
--- app/javascript/chess/Knight.jsx
+++ app/javascript/chess/Knight.jsx
@@ -1,7 +1,40 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { ItemTypes } from './Constants';
+import { DragSource } from 'react-dnd';
 
-export default class Knight extends Component {
-  render() {
-    return <span>♘</span>;
+const knightSource = {
+  beginDrag(props) {
+    return {};
+  }
+};
+
+function collect(connect, monitor) {
+  return {
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging()
   }
 }
+
+class Knight extends Component {
+  render() {
+    const { connectDragSource, isDragging } = this.props;
+    return connectDragSource(
+      <div style={{
+        opacity: isDragging ? 0.5 : 1,
+        fontSize: 25,
+        fontWeight: 'bold',
+        cursor: 'move'
+      }}>
+        ♘
+      </div>
+    );
+  }
+}
+
+Knight.propTypes = {
+  connectDragSource: PropTypes.func.isRequired,
+  isDragging: PropTypes.bool.isRequired
+};
+
+export default DragSource(ItemTypes.KNIGHT, knightSource, collect)(Knight);
