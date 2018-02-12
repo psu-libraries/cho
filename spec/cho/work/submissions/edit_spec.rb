3390183f64bd6e5ebb9c4f81b5fe1cbd844d0134
Adam Wead <amsterdamos@gmail.com>
2018-02-12 11:41:52 -0500

    Don't use work type for edit button, ref #377
    The work type's id was inadvertently being used as the edit button. This
    changes the button to a i18n value for easier configuration. It also
    includes i18n values for all elements of the work views.

--- spec/cho/work/submissions/edit_spec.rb
+++ spec/cho/work/submissions/edit_spec.rb
@@ -9,8 +9,12 @@ RSpec.describe 'Editing works', type: :feature do
   context 'with all the required metadata' do
     it 'updates an existing work with new metadata' do
       visit(edit_work_path(resource))
+      expect(page).to have_content('Editing Work')
+      expect(page).to have_selector('h2', text: 'Work to edit')
+      expect(page).to have_link('Back')
+      expect(page).to have_link('Show')
       fill_in('work_submission[title]', with: 'Updated Work Title')
-      click_button('Update Generic')
+      click_button('Update Work')
       expect(page).to have_content('Updated Work Title')
     end
   end
@@ -19,15 +23,16 @@ RSpec.describe 'Editing works', type: :feature do
     it 'reports errors' do
       visit(edit_work_path(resource))
       fill_in('work_submission[title]', with: '')
-      click_button('Update Generic')
+      click_button('Update Work')
       expect(page).to have_content("Title can't be blank")
+      expect(page).to have_content('1 error prohibited this work from being saved:')
     end
   end
 
   context 'when deleting a work' do
     it 'removes the work from the system' do
       visit(edit_work_path(resource))
-      click_link('Delete Generic')
+      click_link('Delete Work')
       expect(page).to have_content('Work to edit has been deleted')
       expect(Work::Submission.all.count).to eq(0)
       expect(adapter.index_adapter.query_service.find_all.count).to eq(0)
