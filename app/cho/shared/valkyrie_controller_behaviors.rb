8c6dca0cf3c386a60b21e22fb02cfe437a8d33aa
Carolyn Cole <cam156@psu.edu>
2018-02-23 14:56:09 -0500

    Adding Valkyrie Controller Behaviors This extracts some of the common Valkyrie behaviors into a module that can be called by the Valkyrie object controllers

---
+++ app/cho/shared/valkyrie_controller_behaviors.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module ValkyrieControllerBehaviors
+  def load_change_set
+    change_set_class.new(find_resource(params[:id])).prepopulate!
+  end
+
+  def initialize_change_set(**args)
+    change_set_class.new(resource_class.new(args)).prepopulate!
+  end
+
+  def validate_save_and_respond(change_set, error_view)
+    updated_change_set = change_set_persister.validate_and_save(change_set: change_set, resource_params: resource_params)
+    if updated_change_set.errors.blank?
+      respond_success(updated_change_set)
+    else
+      respond_error(updated_change_set, error_view)
+    end
+  end
+
+  def change_set_class
+    raise StandardError.new('This needs to be define in the class including this module')
+  end
+
+  def resource_class
+    raise StandardError.new('This needs to be define in the class including this module')
+  end
+
+  def respond_success(_change_set)
+    raise StandardError.new('This needs to be define in the class including this module')
+  end
+
+  def respond_error(_change_set, _error_view)
+    raise StandardError.new('This needs to be define in the class including this module')
+  end
+end
---
+++ spec/cho/shared/valkyrie_controller_behavior_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ValkyrieControllerBehaviors do
+  before(:all) do
+    class CommonController < ApplicationController
+      include ValkyrieControllerBehaviors
+    end
+  end
+
+  after(:all) do
+    ActiveSupport::Dependencies.remove_constant('CommonController')
+  end
+
+  let(:controller) { CommonController.new }
+
+  describe '#change_set_class' do
+    it 'raises an error' do
+      expect { controller.change_set_class }.to raise_error(StandardError, 'This needs to be define in the class including this module')
+    end
+  end
+
+  describe '#resource_class' do
+    it 'raises an error' do
+      expect { controller.resource_class }.to raise_error(StandardError, 'This needs to be define in the class including this module')
+    end
+  end
+
+  describe '#respond_success' do
+    it 'raises an error' do
+      expect { controller.respond_success(nil) }.to raise_error(StandardError, 'This needs to be define in the class including this module')
+    end
+  end
+
+  describe '#respond_error' do
+    it 'raises an error' do
+      expect { controller.respond_error(nil, nil) }.to raise_error(StandardError, 'This needs to be define in the class including this module')
+    end
+  end
+end
