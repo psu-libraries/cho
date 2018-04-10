04f992fc58fa4cc5ced4e78f04af361b5b023a3a
Adam Wead <amsterdamos@gmail.com>
2018-04-10 16:00:37 -0400

    Install Niftany
    Uses the Niftany gem to lint our code, make changes to match the new
    style regulations.
    
    Excludes changes to sass, which will be part of a separate commit.

--- app/cho/collection/common_fields.rb
+++ app/cho/collection/common_fields.rb
@@ -3,15 +3,15 @@
 module Collection::CommonFields
   extend ActiveSupport::Concern
 
-  Workflow = ['default', 'mediated'].freeze
-  Visibility = ['public', 'authenticated', 'private'].freeze
+  WORKFLOW = ['default', 'mediated'].freeze
+  VISIBILITY = ['public', 'authenticated', 'private'].freeze
 
   included do
     attribute :id, Valkyrie::Types::ID.optional
     attribute :title, Valkyrie::Types::String
     attribute :subtitle, Valkyrie::Types::String
     attribute :description, Valkyrie::Types::String
-    attribute :workflow, Valkyrie::Types::Set.member(Valkyrie::Types::String.enum(*Workflow))
-    attribute :visibility, Valkyrie::Types::Set.member(Valkyrie::Types::String.enum(*Visibility))
+    attribute :workflow, Valkyrie::Types::Set.member(Valkyrie::Types::String.enum(*WORKFLOW))
+    attribute :visibility, Valkyrie::Types::Set.member(Valkyrie::Types::String.enum(*VISIBILITY))
   end
 end
