877e931a6eb0953e1b52a094cd57af69785047d6
Adam Wead <amsterdamos@gmail.com>
2018-04-12 11:06:02 -0400

    Benchmark tests for collections and works
    Refactoring collections benchmark test
    
    Adds tests and creates a new Metrics module.
    
    Test for works with a 50 MB file

---
+++ spec/cho/metrics/collection_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Metrics::Collection do
+  let(:metric) { described_class.new(length: 1, output: report) }
+  let(:report) { StringIO.new }
+  let(:collection) { Collection::Archival.all.first }
+
+  describe '#output' do
+    context 'by default' do
+      subject(:metric) { described_class.new }
+
+      it 'uses a file for the default report' do
+        expect(metric.output.path).to eq('tmp/cho_metrics_collection_10.csv')
+      end
+    end
+
+    context 'with a custom length' do
+      subject(:metric) { described_class.new(length: 100) }
+
+      it 'uses a file with the new length for the default report' do
+        expect(metric.output.path).to eq('tmp/cho_metrics_collection_100.csv')
+      end
+    end
+  end
+
+  it 'creates a collection with works and benchmarks the results' do
+    expect($stdout).to be(STDOUT)
+    metric.run
+    header, results = report.string.split("\n")
+    expect(header).to eq(' User,System,Total,Real')
+    expect(results.split(',').count).to eq(4)
+    expect(collection.members.first.title).to contain_exactly('Sample Work 1')
+    expect($stdout).to be(STDOUT)
+  end
+end
