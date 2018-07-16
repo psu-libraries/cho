02221632347549fa5dde8b5ed6f2f42fa158fb51
mtribone <mtribone@psu.edu>
2018-07-16 14:57:06 -0400

    Removes the screen reader only class and exposes the i18n label to have the file selection elements below the collection datalist.

--- spec/views/work_submissions/new.html.erb_spec.rb
+++ spec/views/work_submissions/new.html.erb_spec.rb
@@ -16,6 +16,7 @@ RSpec.describe 'work/submissions/new', type: :view do
     assert_select 'form[action=?][method=?]', works_path, 'post' do
       assert_select 'input[name=?]', 'work_submission[title]'
       assert_select 'input[name=?]', 'work_submission[member_of_collection_ids][]'
+      assert_select 'label', 'File Selection'
     end
   end
 end
