9a100b438186c9f0c7bd5095de99fe2ac8f1d67d
Trey Pendragon <tpendragon@princeton.edu>
2018-07-30 13:14:42 -0700

    Use Valkyrie's new single-values feature
    Fixes specs and uses Valkyrie's support for single values. Adds
    documentation for schema check and stops locking dry-types/dry-core.
    
    Co-authored-by Adam Wead <amsterdamos@gmail.com>

--- app/valkyrie/memory/singular_persister.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class Memory::SingularPersister < Valkyrie::Persistence::Memory::Persister
-  # overriding ensure_multiple_values! to do nothing since we are allowing singular values
-  def ensure_multiple_values!(resource); end
-end
--- app/valkyrie/solr/singular_persister.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class Solr::SingularPersister < Valkyrie::Persistence::Solr::Persister
-  def repository(resources)
-    Solr::SingularRepository.new(resources: resources, connection: connection, resource_factory: resource_factory)
-  end
-end
