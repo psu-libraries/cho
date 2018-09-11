cc6420e10055f2d9255ac8b32101b4da17509727
Adam Wead <amsterdamos@gmail.com>
2018-09-11 16:23:35 -0400

    Enable code coverage with Code Climate

--- Gemfile
+++ Gemfile
@@ -58,6 +58,7 @@ group :development do
   gem 'listen', '~> 3.0.5'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'travis', require: false
   gem 'web-console', '>= 3.3.0'
   gem 'xray-rails'
 
--- travis/test.sh
+++ travis/test.sh
@@ -7,6 +7,13 @@ mkdir -p dep_cache
 echo "Listing directory contents:"
 ls -l dep_cache
 
+echo -e "\n\n\033[1;33mInstalling Code Climate test reporting tool\033[0m"
+if [ ! -f dep_cache/cc-test-reporter ]; then
+  curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./dep_cache/cc-test-reporter
+  chmod +x ./dep_cache/cc-test-reporter
+fi
+export PATH=$PATH:$(pwd)/dep_cache
+
 echo -e "\n\n\033[1;33mPreparing...\033[0m"
 cp config/travis/hydra-ldap.yml config/hydra-ldap.yml
 cp config/travis/database.yml config/database.yml
@@ -14,8 +21,10 @@ psql -c 'create database travis_ci_test;' -U postgres
 bundle exec solr_wrapper --config config/travis/solr_wrapper_test.yml &
 bin/jetty_wait
 
-echo -e "\n\n\033[1;33mRunning RSpec test suite\033[0m"
+echo -e "\n\n\033[1;33mRunning RSpec test suite with code coverage\033[0m"
+cc-test-reporter before-build
 bundle exec rake cho:travis:rspec
 RSPEC_EXIT_CODE=$?
+cc-test-reporter after-build --exit-code $RSPEC_EXIT_CODE
 
 exit $RSPEC_EXIT_CODE
