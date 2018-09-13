20b032b2fc390067d3645f2c28ebafa072c6bdae
Adam Wead <amsterdamos@gmail.com>
2018-09-13 10:13:26 -0400

    Test for suppressing file and file sets from search results
    Updates search builder to only return the Collections and Works in a search.
    
    Co-authored-by: Adam Wead <amsterdamos@gmail.com>

--- spec/blacklight/catalog_spec.rb
+++ spec/blacklight/catalog_spec.rb
@@ -4,4 +4,38 @@ require 'rails_helper'
 
 RSpec.describe CatalogController, type: :feature do
   it_behaves_like 'a search form', '/catalog'
+
+  context 'when searching for works' do
+    before { create(:work_submission, :with_file) }
+
+    it 'returns the work and excludes file sets and files' do
+      visit(root_path)
+      click_button('Search')
+      within('#documents') do
+        expect(page).to have_link('Sample Generic Work')
+        expect(page).not_to have_content('hello_world.txt')
+        expect(page).not_to have_content(Work::File.all.first.id)
+      end
+    end
+  end
+
+  context 'when searching for collections' do
+    before do
+      create(:archival_collection)
+      create(:curated_collection)
+      create(:library_collection)
+      create(:work_submission, :with_file)
+    end
+    it 'returns the collection and excludes file sets and files' do
+      visit(root_path)
+      click_button('Search')
+      within('#documents') do
+        expect(page).to have_link('Archival Collection')
+        expect(page).to have_link('Curated Collection')
+        expect(page).to have_link('Library Collection')
+        expect(page).not_to have_content('hello_world.txt')
+        expect(page).not_to have_content(Work::File.all.first.id)
+      end
+    end
+  end
 end
