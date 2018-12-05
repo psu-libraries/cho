707581444b44d1e0d1b20bcef4aa67f1a2d5ef6a
Adam Wead <amsterdamos@gmail.com>
2018-12-05 13:59:32 -0500

    Index path to the thumbnail for works and file sets
    Connected to #741
    
    Adds a NullFileSetRepresentative for works that have no file sets, as
    well as adds better test coverage to our existing indexers.

--- app/cho/work/submission_indexer.rb
+++ app/cho/work/submission_indexer.rb
@@ -11,7 +11,8 @@ module Work
       return {} unless resource.try(:work_type_id)
       {
         work_type_ssim: label_for_id,
-        member_of_collection_ssim: collection_labels_for_ids
+        member_of_collection_ssim: collection_labels_for_ids,
+        thumbnail_path_ss: thumbnail_path
       }
     end
 
@@ -21,7 +22,7 @@ module Work
       # @note returns the label associated with the work_type_id
       def label_for_id
         work_type = Work::Type.find(Valkyrie::ID.new(resource.work_type_id))
-        return resource.work_type_id if work_type.nil?
+        return if work_type.nil?
         work_type.label
       end
 
@@ -34,6 +35,12 @@ module Work
 
       def title_field_method
         @title_field ||= DataDictionary::Field.find_using(label: 'title').first.method_name
+      end
+
+      def thumbnail_path
+        thumbnail = resource.representative_file_set.thumbnail
+        return if thumbnail.nil?
+        thumbnail.path.to_s
       end
   end
 end
