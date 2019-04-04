d02d0d1edebf9199c7d6da3af3eb1feb0b848f49
Carolyn Cole <cam156@psu.edu>
2019-04-04 11:13:38 -0400

    Making the test less dependant on the Display Name in the data dictionary

--- spec/cho/work/file_set/show_spec.rb
+++ spec/cho/work/file_set/show_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Work::FileSet, type: :feature do
     visit(polymorphic_path([:solr_document], id: file_set.id))
     expect(page).to have_content 'Part of: Sample Generic Work'
     within('div#document') do
-      expect(page).to have_blacklight_label('title_tesim').with('Title')
+      expect(page).to have_blacklight_label('title_tesim')
       expect(page).to have_blacklight_field('title_tesim').with('Original File Name')
     end
     expect(page).to have_selector('h2', text: 'Files')
