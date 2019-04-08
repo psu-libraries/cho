7ec90e0ea50a7be4174e61430f5e758c6294d6e8
Adam Wead <amsterdamos@gmail.com>
2019-04-08 15:59:39 -0400

    Adding eslint to the build to lint JS files

--- app/javascript/packs/form_fields.js
+++ app/javascript/packs/form_fields.js
@@ -1,10 +1,9 @@
 import FieldSet from '../form_fields/field_set'
 import '../form_fields/styles.css'
 
-let field_sets = document.getElementsByClassName('ff-multiple')
+let fieldSets = document.getElementsByClassName('ff-multiple')
 
-for (let group of field_sets) {
-  let field_set = new FieldSet(group)
-  field_set.register()
+for (let group of fieldSets) {
+  let fieldSet = new FieldSet(group)
+  fieldSet.register()
 }
-
---
+++ config/webpack/loaders/eslint.js
@@ -0,0 +1,11 @@
+const { env } = require('../configuration.js')
+
+module.exports = {
+  enforce: 'pre',
+  test: /\.(js|jsx)$/i,
+  exclude: /node_modules/,
+  loader: 'eslint-loader',
+  options: {
+    failOnError: env.NODE_ENV !== 'production'
+  }
+}
